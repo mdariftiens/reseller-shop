96a87d3f05ce0b353e32ef5d353293f272948070
Md. Arif Bin A. Aziz <md.arif.tiens@gmail.com>
2020-12-20 14:26:29 +0600

    User factory updatede

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,14 +22,14 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        $types = ['admin','manager','delivery-man','customer','subscriber'];
+        $types = ['admin','manager','delivery-man','customer','subscriber'][ $this->faker->numberBetween(0,4)];
 
         $boolValue = (bool) rand(0,1);
 
         return [
             'name' => $this->faker->name,
-            'mobile' => $this->faker->unique()->phoneNumber,
-            'type' => $types[rand(0,4)],
+            'mobile' => $this->faker->unique()->e164PhoneNumber,
+            'type' => $types,
             'is_account_verified' => $boolValue,
             'enabled' => $boolValue,
         ];
