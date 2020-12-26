3439f049b3c684a24b6d9242378acc8ce81204d5
Md. Arif Bin A. Aziz <md.arif.tiens@gmail.com>
2020-12-26 22:15:16 +0600

    abstract controller added

--- app/Abstracts/Http/Controller.php
+++ app/Abstracts/Http/Controller.php
@@ -4,8 +4,11 @@
 namespace App\Abstracts\Http;
 
 use App\Traits\Jobs;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Routing\Controller as BaseController;
 
 
@@ -20,5 +23,27 @@ abstract class Controller extends BaseController
     public function __construct()
     {
 
+    }
+
+
+    /**
+     * Generate a pagination collection.
+     *
+     * @param array|Collection $items
+     * @param int $perPage
+     * @param int $page
+     * @param array $options
+     *
+     * @return LengthAwarePaginator
+     */
+    public function paginate($items, $perPage = 1, $page = null, $options = [])
+    {
+        $perPage = $perPage ?: request('limit', setting('default.list_limit', '25'));
+
+        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+
+        $items = $items instanceof Collection ? $items : Collection::make($items);
+
+        return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
     }
 }
