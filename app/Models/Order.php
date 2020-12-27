a7c514176af397ebc918912cc15169795850e18a
Md. Arif Bin A. Aziz <md.arif.tiens@gmail.com>
2020-12-27 20:18:19 +0600

    fillable and relationship added

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,6 +8,31 @@ class Order extends Model
 {
     public $table = 'orders';
 
-    protected $fillable = ['status', 'deliveryman_user_id', 'created_by_user_id', 'updated_by_usr_id'    ];
+    protected $fillable = [
+        'status',
+        'deliveryman_user_id',
+        'created_by_user_id',
+        'updated_by_usr_id'
+    ];
+
+    public function orderDetail(){
+        return $this->hasMany(OrderDetail::class);
+    }
+
+    public function orderShipping(){
+        return $this->hasOne(OrderShipping::class);
+    }
+
+    public function notes(){
+        return $this->hasMany(Note::class);
+    }
+
+    public function deliveryMan(){
+        return $this->hasOne(User::class,'id','deliveryman_user_id');
+    }
+
+    public function createdBy(){
+        return $this->hasOne(User::class,'id','created_by_user_id');
+    }
 
 }
