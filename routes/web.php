f3b9c0734d78aacc92c4768322cfbccc6e70000b
Md. Arif Bin A. Aziz <md.arif.tiens@gmail.com>
2020-12-27 20:15:36 +0600

    order, product, collection , category route added.

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::middleware('auth',
-    function (){
+Route::middleware('auth', function (){
         Route::get('/home', [App\Http\Controllers\Backend\DashboardController::class, 'index'])->name('home');
         Route::post('/home', [App\Http\Controllers\Backend\DashboardController::class, 'store'])->name('save');
-        Route::get('/test', function (){
-
-        });
-
     });
 
 Auth::routes();
 
-Route::resource('collection', App\Http\Controllers\Backend\CollectionController::class);
-Route::resource('category', App\Http\Controllers\Backend\CategoryController::class);
-Route::resource('product', App\Http\Controllers\Backend\ProductController::class);
+
+Route::prefix('backend')->group(function(){
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('backend.home');
+
+    Route::resource('collection', App\Http\Controllers\Backend\CollectionController::class);
+    Route::resource('category', App\Http\Controllers\Backend\CategoryController::class);
+    Route::resource('product', App\Http\Controllers\Backend\ProductController::class);
+    Route::resource('order', App\Http\Controllers\Backend\OrderController::class);
+
+});
