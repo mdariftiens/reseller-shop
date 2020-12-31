8f1b5a90710888e52d1d7abee06157260bd60e39
Md. Arif Bin A. Aziz <md.arif.tiens@gmail.com>
2020-12-31 23:06:38 +0600

    shop config added.

---
+++ config/shop.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+        'order_status' => [
+            'ORDER_STATUS_NOT_ACCEPTED' => 'NOT_ACCEPTED',
+            'ORDER_STATUS_PENDING' => 'PENDING',
+            'ORDER_STATUS_ON_THE_WAY' => 'ON_THE_WAY',
+            'ORDER_STATUS_DELIVERED' => 'DELIVERED',
+            'ORDER_STATUS_CANCEL' => 'CANCEL',
+            'ORDER_STATUS_RETURNED' => 'RETURNED'
+        ]
+];
