cf2adb201e4712ce336b9a551627f64adfa75ac5
Md. Arif Bin A. Aziz <md.arif.tiens@gmail.com>
2021-01-09 22:09:56 +0600

    Observer methods added

--- app/Abstracts/Observer.php
+++ app/Abstracts/Observer.php
@@ -9,4 +9,20 @@ use Monooso\Unobserve\CanMute;
 abstract class Observer
 {
     use CanMute;
+
+    public function creating(){}
+
+    public function created(){}
+
+    public function editing(){}
+
+    public function edited(){}
+
+    public function updating(){}
+
+    public function updated(){}
+
+    public function deleting(){}
+
+    public function deleted(){}
 }
